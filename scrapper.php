57e7169fee8c2c48718db9c466e0d6f8a52f640f
Kevin Nguyen <KevinNguyenn@utexas.edu>
2015-01-11 22:06:18 -0600

    .

--- scrapper.php
+++ scrapper.php
@@ -4,5 +4,5 @@
 
 	$city = str_replace(" ", "", $city);
 	file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-	preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase"> (.*?) </s', $contents, $matches);
+	preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s', $contents, $matches);
 ?>
