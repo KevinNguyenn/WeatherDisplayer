9104a3f32fac4099f30012678f08e5a7b52d1b79
Kevin Nguyen <KevinNguyenn@utexas.edu>
2015-01-17 18:26:36 -0600

    lol

--- index.php
+++ index.php
@@ -76,7 +76,6 @@
 				<div id="success" class="alert alert-success">Success</div>
 				<div id="fail" class="alert alert-danger">Could not find weather data for that city. Please try again.</div>
 				<div id="noCity" class="alert alert-danger">Please enter a city.</div>
-				<div id="troll" class="alert alert-danger">FUCK HER RIGHT THE PUSSY</div>
 			</div>
 		</div>
 	</div>
@@ -99,7 +98,6 @@
 					
 					if(data =="") {
 						$("#fail").fadeIn();
-						$("#troll").fadeIn();
 					}
 					else {
 						$("#success").html(data).fadeIn();
