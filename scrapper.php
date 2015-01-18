bb677b818466a59f18bbe2e87b76ebf3191a5bd0
Kevin Nguyen <KevinNguyenn@utexas.edu>
2015-01-17 18:15:52 -0600

    update fetcher

--- scrapper.php
+++ scrapper.php
@@ -1,8 +1,18 @@
 <?php
 
-	$city = $_GET['city'];
-
+	$city = $_GET["city"];
 	$city = str_replace(" ", "", $city);
-	$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-	preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s', $contents, $matches);
+	$endpoint = "api.openweathermap.org/data/2.5/weather?q=".$city."&APPID=42af758e0950107323569b8f9b423cf0";
+
+	$session = curl_init($endpoint);
+
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+	$data = curl_exec($session);
+
+	curl_close($session);
+
+	$contents = json_decode($data, true);
+
+	echo $contents['weather'][0]['description'];
 ?>
